7e602c30dda5162a9957a7c631d79e0bd0850e61
momoko-22 <83871733+momoko-22@users.noreply.github.com>
2022-07-22 10:19:25 +0900

    バッジ付与、スタート時間13:30等修正

--- display.js
+++ display.js
@@ -10,11 +10,11 @@ function sortTimestamp(array) {
 }
 
 // const timeline_first_time = 1299794404 //全投稿で一番早い時間
-const timeline_first_time = 1299822360 //最初の画像入りテキストの時間
-let times = 50 //時間経過何倍するか設定 
+// const timeline_first_time = 1299822360 //最初の画像入りテキストの時間
+const timeline_first_time = 1299817800 //13:30
+var times = 14 //時間経過何倍するか設定 
 
 function displayCard_gamemaster(array, classname, start_time, diff) {
-    console.log(times)
     
     //newsクラスの１番上に表示されているidを取得
     //本当はgetElementしたかったけど、子要素が全然取ってこれなかったのでのでとりあえずSessionStorageを使用
@@ -33,9 +33,20 @@ function displayCard_gamemaster(array, classname, start_time, diff) {
     for (i = start_i; i < contents.length; i++) {
         const content = contents[i]
         if (sec < (content.timestamp - timeline_first_time)) {
-            sessionStorage.setItem('displayedLastId_gamemaster',contents[i-1].id)
+            sessionStorage.setItem('displayedLastId_gamemaster', contents[i - 1].id)
             break
         }
+
+        //ゲームマスタータブの横にバッジを付ける
+        const tab = document.getElementById('gamemaster-tab')
+        if (typeof tab.children[0] === 'undefined') {
+            const badge = document.createElement('span')
+            const badgeText = document.createTextNode('NEW')
+            badge.appendChild(badgeText)
+            badge.setAttribute('class', 'badge badge-danger')
+            tab.appendChild(badge)
+        }
+        
 
         if (i == start_i) {
             $('.toast').toast({ delay: 6000 }).toast('show')
@@ -127,10 +138,9 @@ function displayCard_news(array, classname, start_time, diff){
         //画像の設定
         const newImg = document.createElement('img')
         const imgcol = document.createElement('div')
-        console.log(content.imgurl)
         if (content.img != '') {
             newImg.src = content.imgurl
-            newImg.setAttribute('width', '100%')
+            newImg.setAttribute('width', '70%')
             newImg.setAttribute('class', 'bd-placeholder-img')
 
             imgcol.setAttribute('class', 'col-md-4')
@@ -163,7 +173,7 @@ function displayCard_news(array, classname, start_time, diff){
         const Button = document.createElement('a')
         Button.href = '#'
         Button.appendChild(document.createTextNode('クリップ'))
-        Button.setAttribute('class', 'btn btn-primary')
+        Button.setAttribute('class', 'btn btn-primary btn-sm')
         Button.addEventListener('click', clip)
 
         const newid = document.createElement('p')
@@ -221,9 +231,9 @@ function displayCard_tweets(array, classname, start_time, diff) {
             break
         }
 
-        if (content.timestamp > 1299822360) {
-            times = 12
-        }
+        // if (content.timestamp > 1299822360) {
+        //     times = 12
+        // }
 
         if (i == start_i) {
             const tweets = document.getElementById('tweets')
@@ -259,7 +269,7 @@ function displayCard_tweets(array, classname, start_time, diff) {
         const Button = document.createElement('a')
         Button.href = '#'
         Button.appendChild(document.createTextNode('クリップ'))
-        Button.setAttribute('class', 'btn btn-primary')
+        Button.setAttribute('class', 'btn btn-primary btn-sm')
         Button.addEventListener('click', clip)
 
         const newid = document.createElement('p')
@@ -286,7 +296,7 @@ function displayCard_tweets(array, classname, start_time, diff) {
 }
 
 
-function displayCard_clips(array, classname) {
+async function displayCard_clips(array, classname, windowY) {
     $('#'+classname).empty()
      
     const contents = sortTimestamp(array)
@@ -334,12 +344,12 @@ function displayCard_clips(array, classname) {
         const parentDiv = document.getElementById(classname)
         //parentDiv.setAttribute('style','width: 40rem;')
         parentDiv.prepend(newCard)
-    } 
+    }
+    window.scrollTo(0, windowY)
 }
 
-function clip(event) {
+async function clip(event) {
     let text = event.target.parentNode.children[0].textContent
-    console.log(text.indexOf('new'))
     if (text.indexOf('new') == 0) {
         text = text.substring(3)
     }
@@ -355,11 +365,12 @@ function clip(event) {
     clips.push(addobj)
     sessionStorage.setItem('clips', JSON.stringify(clips))
 
+    const windowY = document.documentElement.scrollTop
+    displayCard_clips(clips, "clips", windowY)
+
     //クリップボタンを非表示にする
     const button = event.target.parentNode.children[2]
     button.setAttribute('hidden','') 
-    displayCard_clips(clips, "clips")
-    
 }
 
 function deleteClip(event) {
@@ -375,13 +386,11 @@ function deleteClip(event) {
 }
 
 function restartButtonClick(event) {
-    console.log('button')
-    console.log(event.target)
     let newsTimerID = sessionStorage.getItem('newsTimerID')
     let tweetsTimerID = sessionStorage.getItem('tweetsTimerID')
     let gamemasterTimerID = sessionStorage.getItem('gamemasterTimerID')
     if (newsTimerID == 0) {
-        event.target.innerText = 'タイムライン一時停止' 
+        event.target.innerText = 'タイムラインを一時停止する' 
         const stoptime = sessionStorage.getItem('stoptime')
         const addDiff = (Date.now() - stoptime) / 1000
         diff += addDiff
@@ -395,7 +404,7 @@ function restartButtonClick(event) {
         sessionStorage.setItem('tweetsTimerID',tweetsTimerID)
         sessionStorage.setItem('gamemasterTimerID',gamemasterTimerID)
     } else {
-        event.target.innerText = 'タイムライン再生' 
+        event.target.innerText = 'タイムラインを再生する' 
         const stoptime = Date.now()
         sessionStorage.setItem('stoptime', stoptime)
         clearInterval(newsTimerID)
@@ -405,6 +414,12 @@ function restartButtonClick(event) {
         sessionStorage.setItem('tweetsTimerID',0)
         sessionStorage.setItem('gamemasterTimerID',0)
     }
+}
+
+function clickGamemasterTab(event) {
+    const tab = event.target
+    const badge = tab.children[0]
+    badge.remove()
 }
 
 const starttime = Date.now()
